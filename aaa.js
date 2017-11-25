0e2423f8f99206bead092ced42a034a1f0b1218e
Allison12345 <2562392373@qq.com>
2017-11-25 19:38:19 +0800

    app.js

--- aaa.js
+++ aaa.js
@@ -1,2 +1,16 @@
-Array.from('foo');
-console.log(Array);
+var a = [2, 5, 4, , 4, 6, 2, 0, 99, 4, 66, 34, 67]
+var b = [4, 6, 2, 33, 77, 99, 3, 2, 7, 9]
+function all(a, b) {
+  
+  var tog = []
+  for (var i = 0; i < a.length; i++)
+    for (var j = 0; j < b.length; j++) pos = a[i]
+  poz = b[j]
+  if (pos in b) {
+    tog.push(pos)
+    
+  }
+
+  
+}
+console.log(all(a, b))
--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-var obj = {a:aijiao}
+var names ="Ada,Edith,Gail,Eva,Bess,Jean,Mandy,Angle,fanny"
+var namesArray = names.split(",")
+console.log(namesArray)
+function same(){
+    var sameCap = {}
+    for(var i = 0;i < namesArray.length;i++){
+      if(sameCap[namesArray[i][0]] === undefined){
+        sameCap[namesArray[i][0]] = [namesArray[i]]
+      }else{
+        sameCap[namesArray[i][0]].push(namesArray[i])
+      }
+      
+    }
+    
+    return sameCap
+}
+console.log(same())
