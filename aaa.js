86cf4e44559715bc043e3b192701e67ae4121e87
Allison12345 <2562392373@qq.com>
2017-12-03 15:14:28 +0800

    index.html

--- aaa.js
+++ aaa.js
@@ -1,17 +1,17 @@
-var a = [2, 5, 4, , 4, 6, 2, 0, 99, 4, 66, 34, 67]
+var a = [2, 5, 4, 4, 6, 2, 0, 99, 4, 66, 34, 67]
 var b = [4, 6, 2, 33, 77, 99, 3, 2, 7, 9]
 function all(a, b) {
   var tog = []
-  for (var i = 0; i < a.length; i++){if (b.indexOf(a[i]) === -1) {
-    tog.push(a[i])}
-    for (var j = 0; j < b.length; j++) { if (tog.indexOf(b[j]) === -1) {
-      tog.push(b[j])}
-
-  
+  for (var i = 0; i < a.length; i++) {
+    if (b.indexOf(a[i]) === -1) {
+      tog.push(a[i])
+    }
+    for (var j = 0; j < b.length; j++) {
+      if (tog.indexOf(b[j]) === -1) {
+        tog.push(b[j])
+      }
+    }
   }
- 
-  }
-
   return tog
 }
 console.log(all(a, b))
